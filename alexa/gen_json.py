5b146ccbdc6380eab789e31d3f558868d70f3e5a
Yang Liu <yangliu3456@gmail.com>
2018-06-11 17:02:13 -0400

    june 11 2018

--- alexa/gen_json.py
+++ alexa/gen_json.py
@@ -8,7 +8,7 @@ def gen_json():
     uid = "urn:uuid:" + str(uuid.uuid4())
     updateDate = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.0Z")
     titleText = "Panzoto's Artificial Intelligence News Brief"
-    mainText = "Google released their new paper on Auto Augment, which use Reinforcement Learning to find the best way to augment image data."
+    mainText = "Facebook made a universal music translator that can convert the same rhythm from one instrument to another. Now you can play the theme song of Contra using not just the piano, but also using trumpet and saxophone. For detailed information, follow Panzoto on Twitter."
     # print type(mainText)
     redirectionUrl = "https://data.panzoto.com"
 
